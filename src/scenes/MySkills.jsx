cc8fddeff022f7b630b730c3e11f19559f7ff289
Ninawolfs <nina.wolfs.nw@gmail.com>
2023-05-16 13:26:22 +0200

    update

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,158 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24 bg-deep-blue">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+          Proficient in HTML, CSS, and JavaScript, with a strong foundation in
+            web development. Experience in creating responsive and visually
+            appealing websites using Bootstrap & Tailwind CSS,
+            allowing for rapid development of custom user interfaces with
+            utility-first approach. Skilled in building dynamic and interactive user
+            interfaces using React, with expertise in component-based
+            architecture and state management. Familiar with React Native for
+            cross-platform mobile apps, and adept at API
+            integration for seamless data communication and real-time updates.
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIENCE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Experience
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          My journey as a beginning front-end developer is an exciting and
+            dynamic one. I started by diving into the fundamentals of HTML, CSS,
+            and JavaScript, gradually expanding my knowledge and proficiency in
+            these languages. Along the way, I discovered the power of frameworks
+            like React and Bootstrap, which accelerate development and enhance
+            user interfaces. With each project and challenge overcome, I have
+            gained confidence, problem-solving skills, and a passion for
+            creating engaging web experiences.
+          </p>
+        </motion.div>
+
+        {/* INNOVATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Innovative
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          By staying curious, constantly learning, and embracing emerging
+            trends, you can contribute to shaping the future of digital
+            innovation and drive meaningful change in the online world. As a
+            starting front-end developer, I'm at the forefront of innovation and
+            have the opportunity to bring innovative ideas to life, create
+            immersive user experiences and push the boundaries of web
+            development.
+          </p>
+        </motion.div>
+        {/* IMAGINATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Imaginative
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          Web development and imagination share a profound connection. Web
+            development empowers you to turn imagination into reality, making
+            the digital landscape a more imaginative and vibrant place.Through
+            your imagination, you can envision captivating user interfaces,
+            innovative navigation systems, and engaging animations that
+            captivate and inspire audiences.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
